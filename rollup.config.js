4a85cd397b669f04f5f57b5a7132e616d961a860
François Monniot <f.monniot@samsung.com>
2017-07-19 07:40:50 +0200

    feat: don't include node-zookeeper-client in the library
    and so it's now a peer dependency

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ export default {
   ],
   sourceMap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-  external: [],
+  external: ['node-zookeeper-client'],
   plugins: [
     // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
     commonjs(),
